b2845653c1007bac2ba5a2a0c8b42ff47fc138e3
xiaoyur347 <xiaoyur347@gmail.com>
2020-05-05 18:05:15 +0800

    fix process_vm_readv

--- wrap/process_vm_readv.cpp
+++ wrap/process_vm_readv.cpp
@@ -1,8 +1,10 @@
 #include <sys/uio.h>
+#include <sys/syscall.h>
+#include <unistd.h> // syscall
 
 extern "C" int my_process_vm_readv(long pid, const iovec *lvec,
                               long liovcnt, const void *rvec, long riovcnt,
                               long flags)
 {
-    return 0;
+    return syscall(__NR_process_vm_readv, pid, lvec, liovcnt, rvec, riovcnt, flags);
 }
